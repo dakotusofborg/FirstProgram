58860b81aede10519ffc0200a02ac53e6a1c9276
Dakota Cahill <dakota.cahill@protonmail.com>
2025-01-09 14:13:23 -0500

    added files playing with casting and literals concepts

---
+++ Literals.py
@@ -0,0 +1,18 @@
+#Decimal indicates float literal
+x = 10.0
+y = 17.54312
+z = 12.256
+
+#Boolean literals
+trueLiteral = True
+falseLiteral = False
+
+#String literals
+username = "BenFinkel"
+
+print("username")
+print('username')
+
+
+
+
